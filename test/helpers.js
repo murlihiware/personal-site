1b5ab62348f6cb5a8031083660465cafdc5e5524
murlihiware <murlihiware@gmail.com>
2020-11-29 11:54:19 +0530

    Modified references

--- test/helpers.js
+++ test/helpers.js
@@ -7,27 +7,27 @@ const randomString = (length) => {
 const pages = [
   {
     route: '/',
-    title: 'Michael D\'Angelo',
+    title: 'Murli Hiware',
     heading: 'ABOUT THIS SITE',
   },
   {
     route: '/about',
-    title: 'About | Michael D\'Angelo',
+    title: 'About | Murli Hiware',
     heading: 'ABOUT ME',
   },
   {
     route: '/projects',
-    title: 'Projects | Michael D\'Angelo',
+    title: 'Projects | Murli Hiware',
     heading: 'PROJECTS',
   },
   {
     route: '/stats',
-    title: 'Stats | Michael D\'Angelo',
+    title: 'Stats | Murli Hiware',
     heading: 'STATS',
   },
   {
     route: '/contact',
-    title: 'Contact | Michael D\'Angelo',
+    title: 'Contact | Murli Hiware',
     heading: 'CONTACT',
   },
 ];
