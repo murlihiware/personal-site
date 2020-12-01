2ab20b38a8b1e1f54c8b6406920e0de6d055a854
murlihiware <murlihiware@gmail.com>
2020-12-01 17:46:29 +0530

    restored prev commit

--- app/components/Projects/Cell.js
+++ app/components/Projects/Cell.js
@@ -7,9 +7,12 @@ const Cell = ({ data }) => (
   <div className="cell-container">
     <article className="mini-post">
       <header>
-        <h3>{data.title}</h3>
+        <h3><a href={data.link}>{data.title}</a></h3>
         <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
       </header>
+      <a href={data.link} className="image">
+        <img src={url.resolve(BASE_PATH, data.image)} alt={data.title} />
+      </a>
       <div className="description">
         <p>{data.desc}</p>
       </div>
@@ -20,6 +23,8 @@ const Cell = ({ data }) => (
 Cell.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
     desc: PropTypes.string.isRequired,
   }).isRequired,
